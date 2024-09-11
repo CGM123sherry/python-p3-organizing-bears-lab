72c38c44e11a602d9b3fc91793bfa5cf6bc2a1a1
CGM123sherry <g.christinemurithi@gmail.com>
2024-09-11 12:20:06 +0300

    completed the organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 7, 'M', 'black', 'aggressive', 1);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 5, 'F', 'blonde', 'playful', 1);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 12, 'M', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 8, 'F', 'brown', 'curious', 1);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 6, 'M', 'green', 'grumpy', 1);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 4, 'F', 'white', 'playful', 1);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 3, 'F', 'black', 'shy', 1);
+
